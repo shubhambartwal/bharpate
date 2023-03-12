5fb5da1c615b1b790a7cb2c5fc696635d393fa60
Shubham Bartwal <shubhambartwal58@gmail.com>
2023-03-12 21:58:41 +0530

    backend working created schema for user

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,19 @@
 const mongoose = require('mongoose');
 const mongoURI= 'mongodb+srv://bharpate:Qwerty%40%40123@cluster0.f6jebug.mongodb.net/bharpatemern?retryWrites=true&w=majority';
+mongoose.Promise = global.Promise;
 const mongoDB =async()=>{
-  await mongoose.connect(mongoURI,{useNewUrlParser:true}
+  await mongoose.connect(mongoURI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    //useFindAndModify: false
+}
   ).then(()=>{
     console.log('connected');
-    const fetched_data= mongoose.connection.db.collection("food_items");
-    fetched_data.find({}).toArray(function(err,data){
-      if(err)console.log(err);
-      else console.log(data);
-    })    
+    const fetched_data=  mongoose.connection.db.collection('food_items');
+    fetched_data.find({}).toArray(function (err,data){
+    if(err)console.log(err);
+    else console.log(data);
+  })
   }).catch((error)=>{
     console.log(error);
   });
