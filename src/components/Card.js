7b0e58b6fa035632c24d68473fc3f8332f0c8623
Shubham Bartwal <shubhambartwal58@gmail.com>
2023-03-18 23:20:00 +0530

    added search fun,display data

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,8 @@
 import React from "react";
 
-export default function Card() {
+export default function Card(props) {
+  let options = props.options;
+  let priceOptions= Object.keys(options);
   return (
 
         <div
@@ -9,7 +11,7 @@ export default function Card() {
         >
           <img src="https://upload.wikimedia.org/wikipedia/commons/b/bd/Tandoorimumbai.jpg" className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
+            <h5 className="card-title">{props.foodName}</h5>
             <p className="card-text">This is some important text</p>
             <div className=" container w-100">
               <select className="m-2 h-100  bg-success rounded">
@@ -22,10 +24,13 @@ export default function Card() {
                 })}
               </select>
               <select className="m-2 h-100  bg-success rounded">
-                <option value="half">Half</option>
-                <option value="full">Full</option>
+                {
+                  priceOptions.filter((d)=>d!=="_id").map((data)=>{
+                  return(<option key={data} value={data}>{data}</option>)
+                })}
+              
               </select>
-              <div className="d-inline h-100 ds-5">Total Price</div>
+              <div className="d-inline h-100 fs-6">Total Price</div>
             </div>
           </div>
         </div>
