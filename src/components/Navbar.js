a2911512b1a85c50c283ff37b62c86ae3349f694
Shubham Bartwal <shubhambartwal58@gmail.com>
2023-03-19 21:47:48 +0530

    corrected the navigate in login

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,50 @@
-import React from 'react'
-import {Link} from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <div>
-    <nav className="navbar navbar-expand-lg navbar-dark bg-success">
-  <div className="container-fluid">
-    <Link className="navbar-brand fs-1 fst-italic" to="/">Bharpate</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/login">LogIn</Link>
-        </li>
-        <li className="nav-item">
-        <Link className="nav-link" to="/createuser">SignUp</Link>
-      </li>
-        
-      </ul>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+        <div className="container-fluid">
+          <Link className="navbar-brand fs-1 fst-italic" to="/">
+            Bharpate
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav me-auto ">
+              <li className="nav-item">
+                <Link className="nav-link active fs-5" aria-current="page" to="/">
+                  Home
+                  </Link>
+                  </li>
+                  {(localStorage.getItem("authToken"))?
+                  <li className="nav-item">
+                <Link className="nav-link active fs-5" aria-current="page" to="/">
+                  My Order
+                  </Link>
+                  </li>:""
+                }
+                  </ul>
+            <div className="d-flex">
+              <Link className="btn bg-white text-success m-1" to="/login">
+                LogIn
+              </Link>
+              <Link className="btn bg-white text-success m-1" to="/createuser">
+                SignUp
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
     </div>
-  </div>
-</nav></div>
-  )
+  );
 }
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,12 +1,12 @@
-import React,{useState} from 'react'
-import {Link,useNavigate} from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Login() {
   const navigate = useNavigate();
   const [credentials, setcredentials] = useState({
     password: "",
     email: "",
-   });
+  });
   const handleSubmit = async (e) => {
     e.preventDefault();
     const response = await fetch("http://localhost:5000/api/loginuser", {
@@ -17,54 +17,55 @@ export default function Login() {
       body: JSON.stringify({
         email: credentials.email,
         password: credentials.password,
-       }),
+      }),
     });
     const json = await response.json();
     console.log(json);
     if (!json.success) {
-    alert("Enter valid credentials");
-    if(json.success){
-  localStorage.setItem("authToken",json.authToken);
-console.log(localStorage.getItem("authToken"));
-navigate("/");
+      alert("Enter valid credentials");
     }
-  }};
+      if (json.success) {
+        localStorage.setItem("authToken", json.authToken);
+        console.log(localStorage.getItem("authToken"));
+        navigate("/");
+      }
+  };
   const onChange = (event) => {
-    setcredentials({ ...credentials, [event.target.name]:event.target.value })
-  }
+    setcredentials({ ...credentials, [event.target.name]: event.target.value });
+  };
   return (
     <>
-    <div className="container">
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label className="form-label">Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            aria-describedby="emailHelp"
-            name="email"
-            value={credentials.email}
-            onChange={onChange}
-          />
-        </div>
-        <div className="mb-3">
-          <label className="form-label">Password</label>
-          <input
-            type="password"
-            className="form-control"
-            name="password"
-            value={credentials.password}
-            onChange={onChange}
-          />
-        </div>
-        <button type="submit" className="m-3 btn btn-success">
-          LogIn
-        </button>
-        <Link to="/createuser" className="m-3 btn btn-danger">
-          New User
-        </Link>
-      </form>
-    </div>
-  </>
-  )
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label className="form-label">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              aria-describedby="emailHelp"
+              name="email"
+              value={credentials.email}
+              onChange={onChange}
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              name="password"
+              value={credentials.password}
+              onChange={onChange}
+            />
+          </div>
+          <button type="submit" className="m-3 btn btn-success">
+            LogIn
+          </button>
+          <Link to="/createuser" className="m-3 btn btn-danger">
+            New User
+          </Link>
+        </form>
+      </div>
+    </>
+  );
 }
