aed0b964b70bbdd5f124ca47536d90fcdf7e96ee
Shubham Bartwal <shubhambartwal58@gmail.com>
2023-03-21 12:43:09 +0530

    added add to cart feature

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,38 @@
-import React from "react";
+import React,{useEffect, useRef, useState} from "react";
 import { useDispatchCart,useCart } from "./ContextReducer";
 export default function Card(props) {
+  let data=useCart();
+  let dispatch=useDispatchCart();
   let options = props.options;
   let priceOptions= Object.keys(options);
-  const handleAddToCart=()=>{
+  const [qty,setQty]=useState(1);
+  const[size,setSize]= useState("");
+  const priceRef=useRef();
+  const handleAddToCart=async()=>{
+    await dispatch({type:"ADD",id:props.foodItem._id,
+  name:props.foodItem.name,
+price:finalPrice,
+qty:qty,
+size:size,
+img:props.foodItem.img})
+console.log(data);
   }
+  let finalPrice=qty*parseInt(options[size]);
+  useEffect(()=>{setSize(priceRef.current.value)},
+  [])
   return (
 
     <div
     className="card mt-3"
     style={{ width: "18rem", maxHeight: "360px" }}
   >
-    <img src={props.imgSource} className="card-img-top" alt="..." 
+    <img src={props.foodItem.img} className="card-img-top" alt="..." 
     style={{height:"120px",objectFit:"fill"}}/>
     <div className="card-body">
-      <h5 className="card-title">{props.foodName}</h5>
+      <h5 className="card-title">{props.foodItem.name}</h5>
       <p className="card-text">This is some important text</p>
       <div className=" container w-100">
-        <select className="m-2 h-100  bg-success rounded">
+        <select className="m-2 h-100  bg-success rounded" onChange={(e)=>setQty(e.target.value)}>
           {Array.from(Array(6), (e, i) => {
             return (
               <option key={i + 1} value={i + 1}>
@@ -26,14 +41,14 @@ export default function Card(props) {
             );
           })}
         </select>
-        <select className="m-2 h-100  bg-success rounded">
+        <select className="m-2 h-100  bg-success rounded" ref={priceRef} onChange={(e)=>setSize(e.target.value)}>
           {
             priceOptions.filter((d)=>d!=="_id").map((data)=>{
             return(<option key={data} value={data}>{data}</option>)
           })}
         
         </select>
-        <div className="d-inline h-100 fs-6">Total Price</div>
+        <div className="d-inline h-100 fs-6">₹{finalPrice}/-</div>
       </div>
     </div>
     <hr/>
--- src/components/ContextReducer.js
+++ src/components/ContextReducer.js
@@ -2,7 +2,14 @@ import React, { createContext, useContext, useReducer } from 'react'
 const CartStateContext=createContext();
 const CartDispatchContext= createContext();
 const reducer= (state,action)=>{
-
+switch(action.type)
+{
+    case "ADD":
+        return [...state,{id:action.id,name:action.name,size:action.size,price:action.price,img:action.img,qty:action.qty}]
+        
+        default:
+            console.log("Error in Reducer"); 
+}
 }
 export const CartProvider=({children})=>{
     const[state,dispatch]=useReducer(reducer,[]);
