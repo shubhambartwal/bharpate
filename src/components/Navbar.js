2a2115eb07e90ff3856e2f4cf93ca929d4c33e02
Shubham Bartwal <shubhambartwal58@gmail.com>
2023-03-25 11:22:52 +0530

    modified code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,18 @@
-import React,{useState} from "react";
-import { Link,useNavigate } from "react-router-dom";
-import Cart from "../screens/Cart"
-import Badge from 'react-bootstrap/Badge';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Cart from "../screens/Cart";
+import Badge from "react-bootstrap/Badge";
 import Modal from "../Modal";
 import { useCart } from "./ContextReducer";
 
 export default function Navbar() {
-  let data= useCart();
-  const [cartView,setCartView]=useState(false);
+  let data = useCart();
+  const [cartView, setCartView] = useState(false);
   const navigate = useNavigate();
-  const handleLogout=()=>{
-localStorage.removeItem("authToken");
-navigate('/login');
-  }
+  const handleLogout = () => {
+    localStorage.removeItem("authToken");
+    navigate("/login");
+  };
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-success">
@@ -34,31 +34,57 @@ navigate('/login');
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav me-auto ">
               <li className="nav-item">
-                <Link className="nav-link active fs-5" aria-current="page" to="/">
+                <Link
+                  className="nav-link active fs-5"
+                  aria-current="page"
+                  to="/"
+                >
                   Home
-                  </Link>
-                  </li>
-                  {(localStorage.getItem("authToken"))?
-                  <li className="nav-item">
-                <Link className="nav-link active fs-5" aria-current="page" to="/">
-                  My Order
-                  </Link>
-                  </li>:""
-                }
-                  </ul>
-                  {(!localStorage.getItem("authToken"))?
-            <div className="d-flex">
-              <Link className="btn bg-white text-success m-1" to="/login">LogIn</Link>
-              <Link className="btn bg-white text-success m-1" to="/createuser"> SignUp</Link>
-            </div>
-            :<div >
-            <div className="btn bg-white text-success mx-2" onClick={()=>{setCartView(true)}}>My Cart{"  "}
-            <Badge pill bg="danger">{data ? data.length : 0}</Badge>
-            </div>
-            {cartView ? <Modal onClose={() => setCartView(false)}><Cart></Cart></Modal> : ""}
+                </Link>
+              </li>
+            </ul>
+            {!localStorage.getItem("authToken") ? (
+              <div className="d-flex">
+                <Link className="btn bg-white text-success m-1" to="/login">
+                  LogIn
+                </Link>
+                <Link
+                  className="btn bg-white text-success m-1"
+                  to="/createuser"
+                >
+                  {" "}
+                  SignUp
+                </Link>
+              </div>
+            ) : (
+              <div>
+                <div
+                  className="btn bg-white text-success mx-2"
+                  onClick={() => {
+                    setCartView(true);
+                  }}
+                >
+                  My Cart{"  "}
+                  <Badge pill bg="danger">
+                    {data ? data.length : 0}
+                  </Badge>
+                </div>
+                {cartView ? (
+                  <Modal onClose={() => setCartView(false)}>
+                    <Cart></Cart>
+                  </Modal>
+                ) : (
+                  ""
+                )}
 
-            <div className="btn bg-white text-danger mx-2" onClick={handleLogout} >Logout</div>
-          </div>}
+                <div
+                  className="btn bg-white text-danger mx-2"
+                  onClick={handleLogout}
+                >
+                  Logout
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </nav>
