07f39e9013df74e343cbeb1dd872b29f65612657
Shubham Bartwal <shubhambartwal58@gmail.com>
2023-03-28 16:24:36 +0530

    updated access control

--- backend/index.js
+++ backend/index.js
@@ -4,15 +4,15 @@ const app = express()
 const mongoDB= require('./db');
 const port = 5000
 mongoDB();
-app.use(cors());
-// app.use((req,res,next)=>{
-//   res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin,X-Requested-With,Content-Type,Accept"
-//   );
-//   next();
-// })
+//app.use(cors());
+app.use((req,res,next)=>{
+  res.setHeader("Access-Control-Allow-Origin","https://cerulean-pothos-7c7075.netlify.app");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept"
+  );
+  next();
+})
 app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Hello World!')
