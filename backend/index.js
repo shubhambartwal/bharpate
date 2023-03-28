99a1b46f8c1309f65a5b6323d6bfdb0ed07cd001
Shubham Bartwal <shubhambartwal58@gmail.com>
2023-03-28 17:02:50 +0530

    asscess issue

--- backend/index.js
+++ backend/index.js
@@ -4,15 +4,17 @@ const app = express()
 const mongoDB= require('./db');
 const port = 5000
 mongoDB();
-//app.use(cors());
-app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","https://cerulean-pothos-7c7075.netlify.app");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept"
-  );
-  next();
-})
+app.use(cors());
+// app.use((req,res,next)=>{
+//   res.setHeader("Access-Control-Allow-Origin","https://cerulean-pothos-7c7075.netlify.app");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin,X-Requested-With,Content-Type,Accept"
+//   );
+//   next();
+// })
+app.use(require('morgan')('combined'))
+
 app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Hello World!')
